4c19c3de7f23fe618e3204db65b5905ecefff308
Murilo Fank <53586575+murilofank@users.noreply.github.com>
2023-07-11 03:10:23 -0300

    feat add scripts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app';
 
-app.listen(3333, () => {
-  console.log(`Server is running on port ${3333}.`);
+app.listen(process.env.NODE_ENV, () => {
+  console.log(`Server is running on port ${process.env.NODE_ENV}.`);
 });
